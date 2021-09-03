4f52e1fc594ce511632b948fdcca511e2e58fd3d
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2021-09-04 00:02:14 +0800

    引入上游更新，加入云函数支持

---
+++ sls.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const app = express();
+
+// Routes
+
+app.use(express.static('./'));
+app.get(`/`, (req, res) => {
+  res.redirect("./view/index.html");
+});
+
+/* app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/user', (req, res) => {
+  res.send([
+    {
+      title: 'serverless framework',
+      link: 'https://serverless.com',
+    },
+  ]);
+});
+
+app.get('/user/:id', (req, res) => {
+  const id = req.params.id;
+  res.send({
+    id: id,
+    title: 'serverless framework',
+    link: 'https://serverless.com',
+  });
+}); */
+
+app.get('/404', (req, res) => {
+  res.status(404).send('Not found');
+});
+
+app.get('/500', (req, res) => {
+  res.status(500).send('Server Error');
+});
+
+// Error handler
+app.use(function (err, req, res, next) {
+  console.error(err);
+  res.status(500).send('Internal Serverless Error');
+});
+
+module.exports = app;
